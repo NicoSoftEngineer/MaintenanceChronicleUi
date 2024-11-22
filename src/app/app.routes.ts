25e5f73088ca713ac976a161d3261ffe9317fa02
Nicolas Skuček <nico.skucek@gmail.com>
2024-11-22 09:04:11 +0100

    Updated login page, added register user page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { TenantListComponent } from './components/tenant-list/tenant-list.component';
 import { DefaultComponent } from './layout/default/default.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,7 @@ export const routes: Routes = [
       children: [
         { path: '', component: TenantListComponent, title: 'Tenants' },
         { path: 'login', component: LoginPageComponent, title: 'Login' },
+        { path: 'register', component: RegisterPageComponent, title: 'Register' },
       ],
     }
   ];
--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -3,9 +3,10 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth-service.service';
 import { Router, RouterLink } from '@angular/router';
+import { RegisterUserDto } from '../../models/register-user-dto';
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'app-register-page',
   standalone: true,
   imports: [
     CommonModule,
@@ -14,20 +15,32 @@ import { Router, RouterLink } from '@angular/router';
     ReactiveFormsModule,
     RouterLink,
   ],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
 })
-export class LoginPageComponent {
+export class RegisterPageComponent {
   protected readonly fb = inject(FormBuilder);
   protected readonly authService = inject(AuthService);
   protected readonly router = inject(Router);
-
+  
   protected formular = this.fb.group({
     email: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required, Validators.email],
     }),
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    lastname: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
     password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    passwordConfirm: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required],
     }),
