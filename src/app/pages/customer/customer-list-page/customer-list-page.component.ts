b0d6af62dfdf6b495cd3acf2366f13d569756cae
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-08 19:10:53 +0100

    Added new off canvas component. Implemented filtering by text in customerList

--- src/app/pages/customer/customer-list-page/customer-list-page.component.ts
+++ src/app/pages/customer/customer-list-page/customer-list-page.component.ts
@@ -1,24 +1,32 @@
 import { CustomerListDto } from './../../../models/bussiness/customer/customer-list-dto';
 import { PopUpStateService } from './../../../components/pop-up-modal/pop-up-state.service';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AlertComponent } from '../../../components/alert/alert.component';
 import { PopUpModalComponent } from '../../../components/pop-up-modal/pop-up-modal.component';
 import { initFlowbite } from 'flowbite';
 import { AlertStateService } from '../../../components/alert/alert-state.service';
 import { CustomerService } from '../../../services/customer-service';
+import { OffCanvasComponent } from '../../../components/off-canvas/off-canvas.component';
+import { CustomerFilterDto } from '../../../models/bussiness/customer/customer-filter-dto';
+import { FormInputComponent } from '../../../components/form-input/form-input.component';
 
 @Component({
   selector: 'app-customer-list-page',
-  imports: [RouterLink, AlertComponent, PopUpModalComponent],
+  imports: [RouterLink, AlertComponent, PopUpModalComponent, OffCanvasComponent, FormInputComponent],
   templateUrl: './customer-list-page.component.html',
-  styleUrl: './customer-list-page.component.scss'
+  styleUrl: './customer-list-page.component.scss',
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class CustomerListPageComponent implements OnInit {
   protected readonly customerService = inject(CustomerService);
   protected readonly popUpStateService = inject(PopUpStateService);
   protected readonly alertStateService = inject(AlertStateService);
   protected customers: CustomerListDto[] = [];
+  protected filteredCustomers: CustomerListDto[] = [];
+  protected filter: CustomerFilterDto = {
+    searchText: ''
+  };
 
   constructor() {
     this.loadCustomers();
@@ -37,6 +45,36 @@ export class CustomerListPageComponent implements OnInit {
     this.popUpStateService.openPopUp(`Jste si jistí, že chcete vymazat zákazníka "${customer.name}"`, this.deleteCustomer, customer.id);
   };
   private loadCustomers = () => {
-    this.customerService.getCustomers().subscribe((customers) => {this.customers = customers});
+    this.customerService.getCustomers().subscribe((customers) => {
+      this.customers = customers
+      this.filteredCustomers = customers
+    });
   };
+
+  drawerOpen = false;
+
+  onSave() {
+    // Handle save logic here.
+    console.log('Save clicked!');
+  }
+
+  onClose() {
+    // Handle additional close logic if needed.
+    console.log('Drawer closed');
+  }
+
+  filterItems(){
+    console.log(this.filter.searchText);
+    if(this.filter.searchText){
+      this.filteredCustomers = this.customers.filter((customer) =>
+        customer.name.toLowerCase().includes(this.filter.searchText.toLowerCase()) ||
+        customer.email.toLowerCase().includes(this.filter.searchText.toLowerCase()) ||
+        customer.phoneNumber.toLowerCase().includes(this.filter.searchText.toLowerCase()) ||
+        customer.companyIdNumber.toLowerCase().includes(this.filter.searchText.toLowerCase())
+      );
+      return;
+    }
+    this.filteredCustomers = this.customers;
+    return;
+  }
 }
