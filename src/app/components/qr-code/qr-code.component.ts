697610891868b6df51d6cc6c8f3916b03656392f
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-11 15:26:09 +0100

    Removed html2canvas module

--- src/app/components/qr-code/qr-code.component.ts
+++ src/app/components/qr-code/qr-code.component.ts
@@ -9,7 +9,6 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import html2canvas from 'html2canvas';
 import QRCodeStyling from 'qr-code-styling';
 import QRBorderPlugin, {
   BorderOption,
