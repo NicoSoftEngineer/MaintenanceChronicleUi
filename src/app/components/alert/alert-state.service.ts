99ee000f41c5731794b7140455dcf22e62f125ab
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-20 13:11:41 +0100

    Fixed alert to close when redirecting to another page

--- src/app/components/alert/alert-state.service.ts
+++ src/app/components/alert/alert-state.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -8,6 +9,14 @@ export class AlertStateService implements OnInit {
   private alertState = new BehaviorSubject<{ message: string, type: string; show: boolean }>({ message: '',type: 'success', show: false });
   alertState$ = this.alertState.asObservable();
 
+  constructor(private router: Router) {
+    // Subscribe to router events to close the alert on navigation
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.closeAlert();
+      }
+    });
+  }
   /**
    *
    */
