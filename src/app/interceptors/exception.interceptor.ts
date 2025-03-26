1651395cbd69dc7a69ba9ad5b3b01a06c3a8c558
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-26 18:01:30 +0100

    Added server-error page

--- src/app/interceptors/exception.interceptor.ts
+++ src/app/interceptors/exception.interceptor.ts
@@ -13,6 +13,10 @@ export const exceptionInterceptor: HttpInterceptorFn = (req, next) => {
         // Navigate to the unauthorized page
         router.navigate(['/forbidden']);
       }
+      if (error.status >= 500) {
+        // Navigate to the unauthorized page
+        router.navigate(['/server-error']);
+      }
       // Optionally, add more error handling here
       return throwError(() => error);
     })
