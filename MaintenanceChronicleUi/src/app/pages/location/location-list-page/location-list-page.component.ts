da165431571de4e02e92282453cd79f68f6851d1
Nicolas Skuček <nico.skucek@gmail.com>
2025-04-21 15:38:21 +0200

    Responsive layour added for all location pages

--- MaintenanceChronicleUi/src/app/pages/location/location-list-page/location-list-page.component.ts
+++ MaintenanceChronicleUi/src/app/pages/location/location-list-page/location-list-page.component.ts
@@ -15,10 +15,11 @@ import { FormInputComponent } from '../../../components/form-input/form-input.co
 import { LocationFilter } from '../../../models/bussiness/location/location-filter';
 import { MultiSelect } from '../../../components/multi-select/multi-select.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClickOutsideDirective } from '../../../utils/click-outside.directive';
 
 @Component({
   selector: 'app-location-list-page',
-  imports: [AlertComponent, PopUpModalComponent, RouterLink, OffCanvasComponent, FormInputComponent, MultiSelect, FormsModule, ReactiveFormsModule,],
+  imports: [AlertComponent, PopUpModalComponent, RouterLink, ClickOutsideDirective, OffCanvasComponent, FormInputComponent, MultiSelect, FormsModule, ReactiveFormsModule,],
   templateUrl: './location-list-page.component.html',
   styleUrl: './location-list-page.component.scss',
 })
@@ -32,6 +33,8 @@ export class LocationListPageComponent implements OnInit {
   protected customersInFilterList: CustomerInFilter[] = [];
   protected filter: LocationFilter = { searchText: '', customer: [] };
   protected drawerOpen = false;
+  dropdownOpenFor: string | null = null;
+
 
   constructor() {
     this.loadLocations();
@@ -85,5 +88,17 @@ export class LocationListPageComponent implements OnInit {
 
   isFilterClear(): boolean {
     return this.filter.searchText == '' && this.filter.customer.length === 0;
+  }
+
+  toggleDropdown(id: string): void {
+    if (this.dropdownOpenFor === id) {
+      this.dropdownOpenFor = null;
+    } else {
+      this.dropdownOpenFor = id;
+    }
+  }
+
+  closeDropdown(): void {
+    this.dropdownOpenFor = null;
   }
 }
